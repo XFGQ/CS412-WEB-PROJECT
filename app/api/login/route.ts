c3f5a2d9c7bb068dee5c07dcb7c6a17a23907cb0
XFGQ <furkanduman1925@gmail.com>
2025-12-15 15:34:25 +0100

    initilaze commit

---
+++ app/api/login/route.ts
@@ -0,0 +1,11 @@
+import { type NextRequest } from 'next/server'
+import { db } from '@/db/index'
+import { users } from '@/db/schema'
+
+
+export async function GET(request: NextRequest) {
+    await db.insert(users).values({ username: 'furkan ' })
+    return new Response('login.js!', {
+        status: 200,
+    })
+}
